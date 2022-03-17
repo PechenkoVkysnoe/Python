d7427784f7ae06867b66a97fb8d8f2e1fc35296e
Pechenko <mishagrigorchu@mail.ru>
2022-03-18 01:44:11 +0300

    code review

--- functions.py
+++ functions.py
@@ -1,26 +1,39 @@
-import constants
+from variables import N, K, PUNCTUATION_MARKS, END_MARKS
 
 
-def inlet():
+def input_constants():
     t = 42
+
     while t != 'Y' and t != 'N':
         print("Do you want to enter N and K, enter 'Y/N'")
         t = input()
+
     if t == 'Y':
         print('Enter K: ')
-        constants.k = int(input())
+        k = int(input())
         print('Enter N: ')
-        constants.n = int(input())
+        n = int(input())
+    else:
+        k = K
+        n = N
+
+    return k, n
+
+
+def input_text():
     print("Enter text")
     text = input()
+
     return text
 
 
 def punctuation_marks(text):
-    for i in [';', ':', ',', '"', "'", '_', '@', '(', ')', '*', '&', '–']:
+    for i in PUNCTUATION_MARKS:
         text = text.replace(i, '')
-    for i in ['!', '?', '...', '?!', '!?']:
+
+    for i in END_MARKS:
         text = text.replace(i, '.')
+
     return text
 
 
@@ -28,38 +41,51 @@ def abbreviation(text):
     for i in range(len(text) - 2):
         if text[i] == '.' and not text[i + 2].isupper():
             text = text[0:i] + ' ' + text[i + 1:]
+
     for i in range(len(text) - 4):
-        if (text[i] == 'm' or text[i] == 'M') and text[i + 1] == 'r' and text[i + 2] == '.':
+        if text[i:i + 3] == ('mr.' or 'Mr.'):
             text = text[0:i + 2] + ' ' + text[i + 3:]
+
     for i in range(len(text) - 5):
-        if (text[i] == 'm' or text[i] == 'M') and text[i + 1] == 'r' and text[i + 2] == 's' and text[i + 3] == '.':
+        if text[i:i + 4] == ('mrs.' or 'Mrs.'):
             text = text[0:i + 3] + ' ' + text[i + 4:]
+
     return text
 
 
 def average_median(text):
     list_point = []
+
     for i in range(len(text)):
         if text[i] == '.':
             list_point.append(i)
-    list_word_sentense = []
+
+    list_word_sentence = []
+
     for i in range(len(list_point) - 1):
-        list_word_sentense.append(text.count(' ', list_point[i], list_point[i + 1]) - 1)
-    average = sum(list_word_sentense) / len(list_word_sentense)
-    median = sorted(list_word_sentense)[len(list_word_sentense) // 2]
+        list_word_sentence.append(text.count(' ', list_point[i], list_point[i + 1]) - 1)
+
+    average = sum(list_word_sentence) / len(list_word_sentence)
+    median = sorted(list_word_sentence)[len(list_word_sentence) // 2]
+
     return average, median
 
 
 def number_word(text):
     list_space = []
+
     for i in ['.']:
         text = text.replace(i, '')
+
     text = ' '.join(text.split())
     text = ' ' + text + ' '
+
     word_dict = {}
+
     for i in range(len(text)):
         if text[i] == ' ':
             list_space.append(i)
+
     for i in range(len(list_space) - 1):
         if text[list_space[i] + 1:list_space[i + 1]] in word_dict:
             word_dict[text[list_space[i] + 1:list_space[i + 1]]] += 1
@@ -69,26 +95,31 @@ def number_word(text):
 
 
 def text_preparation():
-    text = abbreviation(punctuation_marks(inlet()))
-    text = text.lower()
+    text = abbreviation(punctuation_marks(input_text())).lower()
     text = text.replace('.', ' . ')
     text = ' '.join(text.split())
     text = '. ' + text + ' '
+
     return text
 
 
-def top_gramms(text):
+def top_grams(text, n):
+    text = text.replace('.', ' ')
     list_word = text.split(' ')
-    gramms = {}
+    grams = {}
+
     for elem in list_word:
-        if len(elem) >= constants.n:
-            for i in range(len(elem) - constants.n + 1):
-                if elem[i:i + constants.n] in gramms:
-                    gramms[elem[i:i + constants.n]] += 1
+        if len(elem) >= n:
+            for i in range(len(elem) - n + 1):
+                if elem[i:i + n] in grams:
+                    grams[elem[i:i + n]] += 1
                 else:
-                    gramms[elem[i:i + constants.n]] = 1
-    list_gramms = sorted(gramms, key=gramms.get, reverse=True)
+                    grams[elem[i:i + n]] = 1
+
+    list_grams = sorted(grams, key=grams.get, reverse=True)
     number = []
-    for i in range(len(list_gramms)):
-        number.append(gramms[list_gramms[i]])
-    return list_gramms, number
+
+    for i in range(len(list_grams)):
+        number.append(grams[list_grams[i]])
+
+    return list_grams, number
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from fun import *
+from functions import text_preparation, number_word, average_median, top_grams, input_constants
 
 
 def main():
+    k, n = input_constants()
     text = text_preparation()
     number_of_words = number_word(text)
 
     for i in number_of_words:
-        print("Number of " + i + " " + str(number_of_words[i]))
+        print(f"Number of {i} {str(number_of_words[i])}")
+    median = average_median(text)[0]
+    average = average_median(text)[1]
+    print(f"Average number of words in a sentence {median}")
+    print(f"Median number of words in a sentence {average}")
 
-    print("Average number of words in a sentence " + str(average_median(text)[0]))
-    print("Median number of words in a sentence " + str(average_median(text)[1]))
-
-    top_of_gramms = top_gramms(text)
-    if len(top_of_gramms[0]) >= constants.k:
-        print('Top ', constants.k)
-        for i in range(constants.k):
-            print("{} {}".format(top_of_gramms[0][i], top_of_gramms[1][i]))
+    top_of_grams = top_grams(text, n)
+    if len(top_of_grams[0]) >= k:
+        print(f"Top {k}")
+        for i in range(k):
+            print(f"{top_of_grams[0][i]} {top_of_grams[1][i]}")
     else:
-        print('Top ', len(top_of_gramms[0]))
-        for i in range(len(top_of_gramms[0])):
-            print("{} {}".format(top_of_gramms[0][i], top_of_gramms[1][i]))
+        print(f"Top {len(top_of_grams[0])}")
+        for i in range(len(top_of_grams[0])):
+            print(f"{top_of_grams[0][i]} {top_of_grams[1][i]}")
 
 
 if __name__ == '__main__':
