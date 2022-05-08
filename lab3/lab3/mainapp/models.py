60e42951e60a7c2395307748c2ed4d97ea168715
Pechenko <mishagrigorchu@mail.ru>
2022-05-08 14:15:13 +0300

    create models

--- lab3/lab3/mainapp/models.py
+++ lab3/lab3/mainapp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import  ContentType
+
+User = get_user_model()
 
 
 # Create your models here.
@@ -38,4 +42,26 @@ class Cart(models.Model):
     owner = models.ForeignKey('Customer', verbose_name="Cart's owner", on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True)
     total_products = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='General Price')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, verbose_name='Customer', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=13, verbose_name='Phone number')
+    address = models.CharField(max_length=255, verbose_name=' Address')
+
+    def __str__(self):
+        return f'Customer: {self.user.first_name} {self.user.last_name}'
+
+
+class Specification(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField
+    name = models.CharField(max_length=255, verbose_name='Product name for specification')
+
+    def __str__(self):
+        return f"Product's specification {self.name}"
 
