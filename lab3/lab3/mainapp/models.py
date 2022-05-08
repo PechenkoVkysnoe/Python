796bfe726b5d13ff441cd9eac5c44d19a2079355
Pechenko <mishagrigorchu@mail.ru>
2022-05-08 14:02:37 +0300

    first commit

---
+++ lab3/lab3/mainapp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Category Name')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Product Name')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Image')
+    description = models.TextField(verbose_name='Description', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Price')
+
+    def __str__(self):
+        return self.title
+
+
+class CartProduct(models.Model):
+    user = models.ForeignKey('Customer', verbose_name='Customer Name', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Cart name', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='Product Name', on_delete=models.CASCADE)
+    quality = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='General Price')
+
+    def __str__(self):
+        return f'Cart product {self.product.title}'
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey('Customer', verbose_name="Cart's owner", on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, blank=True)
+    total_products = models.PositiveIntegerField(default=0)
+
