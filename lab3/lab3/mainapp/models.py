06554d946781a455d9853f55126c32ed901fb1cf
Pechenko <mishagrigorchu@mail.ru>
2022-05-08 15:32:06 +0300

    change arhitections

--- lab3/lab3/mainapp/models.py
+++ lab3/lab3/mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import  ContentType
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -16,6 +17,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Category', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Product Name')
     slug = models.SlugField(unique=True)
@@ -29,8 +33,10 @@ class Product(models.Model):
 
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='Customer Name', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Cart name', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name='Product Name', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Cart name', on_delete=models.CASCADE, related_name='related_product')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     quality = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='General Price')
 
@@ -40,7 +46,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
     owner = models.ForeignKey('Customer', verbose_name="Cart's owner", on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='relate_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='General Price')
 
@@ -55,13 +61,4 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'Customer: {self.user.first_name} {self.user.last_name}'
-
-
-class Specification(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField
-    name = models.CharField(max_length=255, verbose_name='Product name for specification')
-
-    def __str__(self):
-        return f"Product's specification {self.name}"
 
