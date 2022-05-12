cc50539200fd217449df7a71c212714453b99ffe
Pechenko <mishagrigorchu@mail.ru>
2022-05-12 15:35:02 +0300

    repair problems with db

--- lab3/lab3/mainapp/models.py
+++ lab3/lab3/mainapp/models.py
@@ -126,7 +126,7 @@ class Customer(models.Model):
     user = models.ForeignKey(User, verbose_name='Customer', related_name='related_orders', on_delete=models.CASCADE)
     phone = models.CharField(max_length=13, verbose_name='Phone number', null=True, blank=True)
     address = models.CharField(max_length=255, verbose_name=' Address', null=True, blank=True)
-    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_customer')
+    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_customer', blank=True, null=True)
     def __str__(self):
         return f'Customer: {self.user.first_name} {self.user.last_name}'
 
